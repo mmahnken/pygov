13c43a306d58c601e7f241a11e665c45f8d16518
Meggie Mahnken <mmm25eg@gmail.com>
2013-04-27 15:11:32 -0700

    added emergency shelter acquisition

--- ACDataClient.py
+++ ACDataClient.py
@@ -1,8 +1,8 @@
 import requests
 
 class ACDataClient(object):
-	def __init__(self, url):
-		self.url = url
+	def __init__(self):
+		return
 
 	def get_parks(self):
 		url = "https://data.acgov.org/id/k9se-aps6.json?"
@@ -13,3 +13,13 @@ class ACDataClient(object):
 		url = "https://data.acgov.org/id/xzcz-7uep.json?"
 		response = requests.get(url)
 		return response.json()
+
+	def get_crime_reports(self):
+		url = "https://data.acgov.org/id/8khs-56kk.json?"
+		response = requests.get(url)
+		return response.json()
+
+	def get_shelters(self):
+		url = "https://data.acgov.org/id/wu33-vg7m.json?"
+		response = requests.get(url)
+		return requests.get()
